e50f5a91338714fcf13f87ab71baf06769dcabe6
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-07-16 18:26:17 -0400

    start conversation button works

--- lib/screens/main/chat.dart
+++ lib/screens/main/chat.dart
@@ -14,13 +14,14 @@ final String healthz = "http://localhost:8080/healthz";
 final String mNewConvo = "http://localhost:8080/m/new/unstructured";
 
 Future<bool> healthCheck() async {
+  print("healthCheck");
   try {
     final response = await http.get(Uri.parse(healthz));
     final code = response.statusCode;
-    print("healthz: $code");
+    print("\t/healthz: $code");
     return (code == 200);
   } catch (e) {
-    print("Error: $e");
+    print("\tError: $e");
     return false;
   }
 }
@@ -35,7 +36,7 @@ enum ConvoState { ready, started, failed, done }
 class _ChatScreenState extends State<ChatScreen> {
   // Resource state
   bool isRecording = false;
-  bool hasPermissions = false;
+  bool haveMicPermissions = false;
   bool isServerHealthy = false;
   // Conversation state
   ConvoState convoState = ConvoState.ready;
@@ -47,31 +48,24 @@ class _ChatScreenState extends State<ChatScreen> {
   final int sampleRate = 44100;
   final int numChannels = 1;
 
-  void buttonClicked(String whichButton) {
-    print('Button clicked: $whichButton');
-  }
-
   void getPermissions() async {
     String? errorMessage;
     print('ChatScreen GetPermissions clicked');
     print('ChatScreen getting / checking microphone permission');
     bool gotPermission = await record.hasPermission();
     setState(() {
-      hasPermissions = gotPermission;
+      haveMicPermissions = gotPermission;
     });
   }
 
   /// Acquire the media resource and begin recording from it.
-  void startRecording(BuildContext context) async {
-    String? errorMessage;
-    print('ChatScreen start button clicked');
-    print('ChatScreen getting / checking microphone permission');
-    bool hasPermission = await record.hasPermission();
-    if (!hasPermission) {
-      print('ChatScreen failed to get microphone permission.');
-      errorMessage = "Chat requires microphone permissions.";
+  /// Return error message.
+  Future<String?> startRecording(BuildContext context) async {
+    bool micPerm = await record.hasPermission();
+    if (!micPerm) {
+      return "Microphone permissions required for chat. Please enable in your system settings.";
     } else {
-      print('ChatScreen got microphone permission!');
+      print("\tStarting to record...");
       await record.start(
         bitRate: bitRate,
         samplingRate: sampleRate,
@@ -80,154 +74,100 @@ class _ChatScreenState extends State<ChatScreen> {
       setState(() {
         isRecording = true;
       });
-    }
-    if (errorMessage != null) {
-      util.showError(context, errorMessage);
+      print("\tRecording started.");
     }
   }
 
-  /// Release the media resource and send the recording to the APIa
-  void stopRecording(BuildContext context) async {
-    String? errorMessage;
+  /// Release the media resource
+  /// Return error string
+  Future<String?> stopRecording() async {
     String? audioPath = await record.stop();
-    if (audioPath != null) {
-      // TODO send to the API
-      print("Got audioPath: $audioPath");
-      await player.play(audioPath);
-      // await player.play(DeviceFileSource(audioPath));
-    } else {
-      print("ChatScreen Error: failed to get audioPath from record.stop()");
-      errorMessage = "An error occurred, please try again.";
+    if (audioPath == null) {
+      return "An error occurred while listening, please try again.";
     }
     setState(() {
       isRecording = false;
     });
-    if (errorMessage != null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(errorMessage)),
-      );
-    }
+  }
+
+  /// Send the recording to the API
+  Future<void> sendUtterance(String audioPath) async {
+    print("Got audioPath: $audioPath");
+    await player.play(audioPath);
+    // TODO send to the API
+    // await player.play(DeviceFileSource(audioPath));
   }
 
   // Check API health, get mic. permissions, create conversation in Firestore
-  Future<void> startNewConversation(BuildContext context) async {
+  Future<String?> startNewConversation(BuildContext context) async {
     print("startNewConversation");
+    String? msg;
+    final bool micPerm = await record.hasPermission();
+    setState(() {
+      haveMicPermissions = micPerm;
+    });
+    if (!micPerm) {
+      return "Microphone permissions required for chat. Please enable in your system settings.";
+    } else {
+      print("\tmicrophone permissions allowed");
+      await record.dispose();
+    }
     final bool healthz = await healthCheck();
-    (healthz)
-      ? print("Moshi API healthy.")
-      : print("Moshi API unhealthy, please try again.");
-
-          // ElevatedButton(  // healthcheck
-          //   style: ElevatedButton.styleFrom(
-          //     padding: EdgeInsets.all(8.0),
-          //   ),
-          //   child: Text(
-          //     isServerHealthy ? 'Server healthy' : 'Check server health',
-          //     style: TextStyle(fontSize: 18.0),
-          //   ),
-          //   onPressed: () async {
-          //     String healthMsg;
-          //     if (await healthCheck()) {
-          //       healthMsg = "Moshi API healthy.";
-          //     } else {
-          //       healthMsg = "Moshi API unhealthy, please try again.";
-          //     }
-          //     final snackBar = SnackBar(content: Text(healthMsg));
-          //     ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          //   },
-          // ),
-          // ElevatedButton(  // get audio permissions
-          //   style: ElevatedButton.styleFrom(
-          //     padding: EdgeInsets.all(8.0),
-          //   ),
-          //   child: Text(
-          //     hasPermissions ? 'Audio access established' : 'Get microphone',
-          //     style: TextStyle(fontSize: 18.0),
-          //   ),
-          //   onPressed: hasPermissions ? null : getPermissions,
-          // ),
-          // ElevatedButton(  // start new conversation
-          //   style: ElevatedButton.styleFrom(
-          //     padding: EdgeInsets.all(8.0),
-          //   ),
-          //   child: Text(
-          //     (convoState == ConvoState.started)
-          //       ? "End conversation"
-          //       : "Start a conversation",
-          //     style: TextStyle(fontSize: 18.0),
-          //   ),
-          //   onPressed: () => startNewConversation(context),
-          // ),
-          // GestureDetector(  // TODO error handling for recorder being in wrong state when button up/down
-          //   onTapDown: (_) {
-          //     startRecording(context);
-          //   },
-          //   onTapUp: (_) {
-          //     record.isRecording().then((isRecording) {
-          //       if (isRecording) {
-          //         stopRecording(context);
-          //       }
-          //     });
-          //   },
-          //   onTapCancel: () {
-          //     record.isRecording().then((isRecording) {
-          //       if (isRecording) {
-          //         stopRecording(context);
-          //       }
-          //     });
-          //   },
-          //   // child: Center(
-          //   //   child: ElevatedButton(
-          //   //     style: ElevatedButton.styleFrom(
-          //   //       padding: EdgeInsets.all(8.0),
-          //   //     ),
-          //   //     child: Text(
-          //   //       isRecording ? 'Recording...' : 'Hold to chat',
-          //   //       style: TextStyle(fontSize: 18.0),
-          //   //     ),
-          //   //     onPressed: () => print("Recording button clicked"),
-          //   //   ),
-          //   // ),
-          // ),
+    setState(() {
+      isServerHealthy = healthz;
+    });
+    if (!healthz) {
+      return "Moshi servers unhealthy, please try again.";
+    } else {
+      print("\tmoshi api healthy");
+    }
+    final authService = Provider.of<AuthService>(context, listen: false);
+    final String? erMsg = await getNewConversation(authService);
+    if (erMsg != null) {
+      return erMsg;
+    }
   }
 
   /// Create a new Conversation document in the backend and set local CID
-  Future<void> getNewConversation(BuildContext context) async {
+  /// Return an error message if one occurred.
+  Future<String?> getNewConversation(AuthService authService) async {
     print("getNewConversation");
-    final authService = Provider.of<AuthService>(context, listen: false);
     final user = authService.currentUser!;
     final token = await user.getIdToken();
-    String? _cid;
-    String msg = "An error occurred, please try again.";
     try {  // network request; json decoding
       final response = await http.get(
         Uri.parse(mNewConvo),
         headers: {HttpHeaders.authorizationHeader: "Bearer $token"},
       );
       final code = response.statusCode;
-      print("\tm/new/unstructured: $code");
+      print("\t/m/new/unstructured: $code");
       if (code == 200) {
         Map<String, dynamic> json = jsonDecode(response.body);
-        msg = json['message'];
-        _cid = json['detail']['conversation_id'];
+        final String msg = json['message'];
+        final String _cid = json['detail']['conversation_id'];
         print("\tmsg: $msg");
         print("\tcid: $_cid");
         setState(() {
           cid = _cid;
+          convoState = ConvoState.started;
         });
+        return null;
       } else if (code == 401) {
-        print("\tinvalid token");  // TODO refresh the user token
+        // NOTE https://firebase.google.com/docs/auth/flutter/start persisting auth state on web
+        print("\tinvalid token");  // TODO refresh the user token? Firebase says tokens are long lived: https://firebase.google.com/docs/auth/admin/manage-sessions
+        print("\tTODO");
+        return "Need to refresh authentication tokens, please try again.";
+      } else {
+        return "Server error: $code";
       }
     } catch (e) {
       print("\tError: $e");
+      return "An error occurred, please try again.";
     }
-    final snackBar = SnackBar(content: Text(msg));
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO use the errorService to show users error messages
     return Container(
       child: Stack(
         children: [
@@ -239,15 +179,18 @@ class _ChatScreenState extends State<ChatScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "Microphone permissions: $hasPermissions",
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  Text(
-                    "$convoState: ${cid?.substring(0, 8) ?? 'inactive'}",
+                    "Microphone permissions: $haveMicPermissions",
                     style: TextStyle(fontSize: 16.0),
                   ),
                   Text(
                     "Recording: $isRecording",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  Text(
+                    convoState.toString(),
+                    // (convoState != ConvoState.started)
+                    //   ? "Conversation: ${cid?.substring(0, 8)}"
+                    //   : "Conversation: inactive",
                     style: TextStyle(fontSize: 16.0),
                   ),
                 ],
@@ -258,7 +201,10 @@ class _ChatScreenState extends State<ChatScreen> {
             child: FloatingActionButton.extended(  // Start convo
               onPressed: () async {
                 if (convoState != ConvoState.started) {
-                  await startNewConversation(context);
+                  final String? erMsg = await startNewConversation(context);
+                  if (erMsg != null) {
+                    util.showError(context, erMsg);
+                  }
                 } else {
                   print("startButton\n\tconvoState: $convoState");
                   print("TODO");
@@ -275,6 +221,41 @@ class _ChatScreenState extends State<ChatScreen> {
                   : Icons.restart_alt,
               ),
               backgroundColor: Colors.pink,
+            ),
+          ),
+          Positioned(
+            bottom: 48.0,
+            right: 128.0,
+            child: GestureDetector(
+              // child: Icon(Icons.add),
+              // TODO error handling for recorder being in wrong state when button up/down
+              onTapDown: (_) {
+                print("\tonTapDown");
+                // startRecording(context);
+              },
+              onTapUp: (_) {
+                print("\tonTapUp");
+                // record.isRecording().then((isRecording) {
+                //   if (isRecording) {
+                //     stopRecording(context);
+                //   }
+                // });
+              },
+              onTapCancel: () {
+                print("\tonTapCancel");
+                // record.isRecording().then((isRecording) {
+                //   if (isRecording) {
+                //     stopRecording(context);
+                //   }
+                // });
+              },
+              child: FloatingActionButton(
+                onPressed: () => print("\tonPressed"),
+                // onPressed:  ? null : () => print("\tonPressed"),
+                child: Icon(Icons.add),
+                // NOTE onPressed null doesn't disable button: https://github.com/flutter/flutter/issues/107480
+                backgroundColor: (convoState != ConvoState.started) ? Colors.grey : Colors.pink,
+              ),
             ),
           ),
         ],
--- lib/util.dart
+++ lib/util.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 Future<void> showError(BuildContext context, String msg) async {
+  print("Error: $msg");
   final snackBar = SnackBar(content: Text(msg));
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
 }
