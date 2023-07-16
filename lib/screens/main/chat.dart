ae26c83cb4221fa1fee3bf9f2b59bbe051557326
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-07-16 15:58:30 -0400

    wip start button

--- lib/screens/main/chat.dart
+++ lib/screens/main/chat.dart
@@ -137,94 +137,14 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  // Check API health, get mic. permissions, create conversation in Firestore
   Future<void> startNewConversation(BuildContext context) async {
     print("startNewConversation");
-  }
+    final bool healthz = await healthCheck();
+    (healthz)
+      ? print("Moshi API healthy.")
+      : print("Moshi API unhealthy, please try again.");
 
-  /// Create a new Conversation document in the backend and set local CID
-  Future<void> getNewConversation(BuildContext context) async {
-    print("getNewConversation");
-    final authService = Provider.of<AuthService>(context, listen: false);
-    final user = authService.currentUser!;
-    final token = await user.getIdToken();
-    String? _cid;
-    String msg = "An error occurred, please try again.";
-    try {  // network request; json decoding
-      final response = await http.get(
-        Uri.parse(mNewConvo),
-        headers: {HttpHeaders.authorizationHeader: "Bearer $token"},
-      );
-      final code = response.statusCode;
-      print("\tm/new/unstructured: $code");
-      if (code == 200) {
-        Map<String, dynamic> json = jsonDecode(response.body);
-        msg = json['message'];
-        _cid = json['detail']['conversation_id'];
-        print("\tmsg: $msg");
-        print("\tcid: $_cid");
-        setState(() {
-          cid = _cid;
-        });
-      } else if (code == 401) {
-        print("\tinvalid token");  // TODO refresh the user token
-      }
-    } catch (e) {
-      print("\tError: $e");
-    }
-    final snackBar = SnackBar(content: Text(msg));
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Microphone permissions: $hasPermissions",
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  Text(
-                    "Conversation: ${cid?.substring(0, 8) ?? 'inactive'}",
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  Text(
-                    "Recording: $isRecording",
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Center(
-            child: FloatingActionButton.extended(  // Start convo
-              onPressed: () async {
-                if (convoState == ConvoState.ready) {
-                  await startNewConversation(context);
-                } else {
-                  print("TODO");
-                }
-              },
-              label: Text(
-                (convoState == ConvoState.ready)
-                  ? 'Start conversation'
-                  : 'Restart conversation',
-              ),
-              icon: Icon(
-                (convoState == ConvoState.ready)
-                  ? Icons.start
-                  : Icons.restart_alt,
-              ),
-              backgroundColor: Colors.pink,
-            ),
-          ),
           // ElevatedButton(  // healthcheck
           //   style: ElevatedButton.styleFrom(
           //     padding: EdgeInsets.all(8.0),
@@ -297,6 +217,94 @@ class _ChatScreenState extends State<ChatScreen> {
           //   //   ),
           //   // ),
           // ),
+  }
+
+  /// Create a new Conversation document in the backend and set local CID
+  Future<void> getNewConversation(BuildContext context) async {
+    print("getNewConversation");
+    final authService = Provider.of<AuthService>(context, listen: false);
+    final user = authService.currentUser!;
+    final token = await user.getIdToken();
+    String? _cid;
+    String msg = "An error occurred, please try again.";
+    try {  // network request; json decoding
+      final response = await http.get(
+        Uri.parse(mNewConvo),
+        headers: {HttpHeaders.authorizationHeader: "Bearer $token"},
+      );
+      final code = response.statusCode;
+      print("\tm/new/unstructured: $code");
+      if (code == 200) {
+        Map<String, dynamic> json = jsonDecode(response.body);
+        msg = json['message'];
+        _cid = json['detail']['conversation_id'];
+        print("\tmsg: $msg");
+        print("\tcid: $_cid");
+        setState(() {
+          cid = _cid;
+        });
+      } else if (code == 401) {
+        print("\tinvalid token");  // TODO refresh the user token
+      }
+    } catch (e) {
+      print("\tError: $e");
+    }
+    final snackBar = SnackBar(content: Text(msg));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO use the errorService to show users error messages
+    return Container(
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Microphone permissions: $hasPermissions",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  Text(
+                    "$convoState: ${cid?.substring(0, 8) ?? 'inactive'}",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  Text(
+                    "Recording: $isRecording",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: FloatingActionButton.extended(  // Start convo
+              onPressed: () async {
+                if (convoState != ConvoState.started) {
+                  await startNewConversation(context);
+                } else {
+                  print("startButton\n\tconvoState: $convoState");
+                  print("TODO");
+                }
+              },
+              label: Text(
+                (convoState != ConvoState.started)
+                  ? 'Start conversation'
+                  : 'Restart conversation',
+              ),
+              icon: Icon(
+                (convoState != ConvoState.started)
+                  ? Icons.touch_app
+                  : Icons.restart_alt,
+              ),
+              backgroundColor: Colors.pink,
+            ),
+          ),
         ],
       ),
     );
