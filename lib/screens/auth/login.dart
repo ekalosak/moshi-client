e694fb19520c3d89ec3abffa521a0d3f632c67ac
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-07-23 10:34:42 -0400

    rearranged the login page, stylized it; todo the sign up and password reset accordingly

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -3,115 +3,151 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import '../../services/auth.dart';
-import '../main/main.dart';
-import 'sign_up.dart';
-import 'password_reset.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  String? err;
 
-  Future<void> loginWithEmailPassword(BuildContext context) async {
-    final AuthService authService =
-        Provider.of<AuthService>(context, listen: false);
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<String?> loginWithEmailPassword(AuthService authService) async {
     final String? authToken = await authService.signInWithEmailAndPassword(
       emailController.text,
       passwordController.text,
       context,
     );
-
     if (authToken != null) {
-      print("Login with email+password succeded!");
-      context.go('/');
+      return null;
     } else {
-      print(
-          "Login with email+password failed."); // NOTE the authService handles the popups for user info
+      return "Login with email+password failed.";
     }
   }
 
-  // Future<void> signupWithEmailPassword(BuildContext context) async {
-  //  context.go('/a/signup');
+  // Future<void> loginWithGoogle(BuildContext context) async {
+  //   final AuthService authService = Provider.of<AuthService>(context, listen: false);
+  //   final String? authToken = await authService.signInWithGoogle(context);
+  //   if (authToken != null) {
+  //     print("Login with google succeded!");
+  //     context.go('/m');
+  //   } else {
+  //     print("Login with google failed.");
+  //   }
   // }
-
-  Future<void> loginWithGoogle(BuildContext context) async {
-    final AuthService authService =
-        Provider.of<AuthService>(context, listen: false);
-    final String? authToken = await authService.signInWithGoogle(context);
-
-    if (authToken != null) {
-      print("Login with google succeded!");
-      context.go('/m');
-    } else {
-      print("Login with google failed.");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    final AuthService authService = Provider.of<AuthService>(context, listen: false);
+    if (err != null) {
+      // If err isn't null, show a snackbar with the error
+      WidgetsBinding.instance!.addPostFrameCallback((_) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(err!)),
+        );
+        setState(() {
+          err = null;
+        });
+      });
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
+        child: LayoutBuilder(builder: (context, constraints) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    FloatingActionButton.extended(
+                      heroTag: "signup",
+                      label: Text('Sign up'),
+                      icon: Icon(Icons.person_add),
+                      onPressed: () {
+                        context.go('/a/signup');
+                      },
+                    ),
+                  ],
+                ),
               ),
-              autofillHints: [AutofillHints.email],
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                      ),
+                      autofillHints: [AutofillHints.email],
+                    ),
+                    TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                      autofillHints: [AutofillHints.password],
+                    ),
+                  ],
+                ),
               ),
-              obscureText: true,
-              autofillHints: [AutofillHints.password],
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              child: Text('Log in'),
-              onPressed: () {
-                loginWithEmailPassword(context);
-              },
-            ),
-            SizedBox(height: 10),
-            Text("- or -"),
-            SizedBox(height: 10),
-            ElevatedButton(
-              child: Text('Sign up'),
-              onPressed: () {
-                // context.go('/a/signup');
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => SignUpScreen()),
-                );
-              },
-            ),
-            SizedBox(height: 10),
-            Text("- or -"),
-            SizedBox(height: 10),
-            ElevatedButton(
-              child: Text('Reset password'),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => PasswordResetScreen()),
-                );
-              },
-            )
-            // TODO add teh clientId
-            // ElevatedButton(
-            //   child: Text('Log in with Google'),
-            //   onPressed: () {
-            //     loginWithGoogle(context);
-            //   },
-            // ),
-          ],
-        ),
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: EdgeInsets.all(24),
+                  child: FloatingActionButton.extended(
+                    heroTag: "login",
+                    label: Text('Log in'),
+                    icon: Icon(Icons.login),
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    onPressed: () async {
+                      err = await loginWithEmailPassword(authService);
+                      setState(() {
+                        err = err;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton.extended(
+                        heroTag: "reset",
+                        label: Text('Reset password'),
+                        icon: Icon(Icons.lock),
+                        onPressed: () {
+                          context.go('/a/reset');
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
