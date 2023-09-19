c5b61faba28370b2f1ed84cfc7b0c4537186b0d6
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-09-19 16:28:37 -0400

    marketing version to 23.9.4

--- lib/screens/main/wrapper.dart
+++ lib/screens/main/wrapper.dart
@@ -12,7 +12,7 @@ import 'feed.dart';
 import 'profile.dart';
 import 'transcripts.dart';
 
-const String version = "23.9.3";
+const String version = "23.9.4";
 
 class WrapperScreen extends StatefulWidget {
   // make WrapperScreen take User user as a param
